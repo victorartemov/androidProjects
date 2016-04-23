ee5cd6f492c5073b49cf788bf41309d7feeef4fd
Victor Artemov <victor_artemov@outlook.com>
2016-04-23 23:28:20 +0300

    Created an AsyncTask class in GetReady.java to improve the way the two threads were interacting before

--- DemoApp/app/src/main/java/com/mycompany/demoapp/GetReady.java
+++ DemoApp/app/src/main/java/com/mycompany/demoapp/GetReady.java
@@ -1,29 +1,22 @@
 package com.mycompany.demoapp;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v4.media.MediaBrowserCompat;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.LogRecord;
-import android.os.Message;
-import android.os.Handler;
 
 public class GetReady extends AppCompatActivity {
-    TextView bigRedNumber;
-    Animation anim;
-    String buf;
+    private TextView bigRedNumber;
+    private Animation anim;
+    private volatile String buf;
+    private int mDelay = 1000;
 
-    boolean backButtonWasPressed;
+    ShowBigRedNumbersTask task = new ShowBigRedNumbersTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,85 +25,80 @@ public class GetReady extends AppCompatActivity {
 
         bigRedNumber = (TextView) findViewById(R.id.bigRedNumber);
         bigRedNumber.setTextColor(Color.RED);
+        bigRedNumber.setVisibility(TextView.INVISIBLE);
         anim = AnimationUtils.loadAnimation(this,R.anim.myscale);
-        backButtonWasPressed = false;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        /*ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-        Runnable job = new Runnable() {
-            int cnt = 3;
-            @Override
-            public void run() {
-                buf = "" + cnt;
-                h.sendEmptyMessage(0);
-                cnt--;
-            }
-        };
-        service.scheduleAtFixedRate(job, 0, 1, TimeUnit.SECONDS);
-        try {
-            service.awaitTermination(3, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        service.shutdownNow();*/
-
-        Thread updatingBigRedNumbers = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-
-                        buf = "3";
-                        h.sendEmptyMessage(0);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        buf = "2";
-                        h.sendEmptyMessage(0);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        buf = "1";
-                        h.sendEmptyMessage(0);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        if(!backButtonWasPressed) {
-                            Intent i = new Intent(GetReady.this, GamePage.class);
-                            startActivity(i);
-                        }
-
-                    }
-                }
-        );
-        updatingBigRedNumbers.start();
+        task.execute();
     };
 
-    Handler h = new Handler() {
+    class ShowBigRedNumbersTask extends AsyncTask<Void, String, Void> {
+
+        private boolean isStopped = false;
+
         @Override
-        public void handleMessage(Message msg) {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            bigRedNumber.setVisibility(TextView.VISIBLE);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while(!isCancelled() && !isStopped) {
+                for (int i = 3; i > 0; i--) {
+                    buf = String.valueOf(i);
+                    publishProgress(buf);
+                    sleep();
+                    if(i == 1) {
+                      stop();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
             bigRedNumber.setText(buf);
             bigRedNumber.startAnimation(anim);
         }
-    };
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Intent intent = new Intent(GetReady.this, GamePage.class);
+            startActivity(intent);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            this.stop();
+        }
+
+        private void sleep() {
+            try {
+                Thread.sleep(mDelay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void stop() {
+            isStopped = true;
+        }
+    }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         Intent i = new Intent(this,FirstPage.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        backButtonWasPressed = true;
+        task.cancel(true);
         startActivity(i);
     }
 }
